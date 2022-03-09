ecabba2aa0e51d35324be2eff0e39e603a88557c
Jonathan's Mac Mini <jonathansmacmini@Jonathanss-Mini.attlocal.net>
2022-03-09 06:15:52 -0600

    pseudo code for js and structural code for html/css

---
+++ script.js
@@ -0,0 +1,91 @@
+// cCde Quiz
+//Dom Variables
+const startGameBtn = .document.getElementById('startGameBtn');
+const question = .document.getElementById('question');
+const quiz = .document.getElementById('quiz');
+const counter = .document.getElementById('counter');
+const timeGauge = .document.getElementById('timeGauge');
+const answerA = .document.getElementById('A');
+const answerB = .document.getElementById('B');
+const answerC = .document.getElementById('C');
+
+//Store user info
+var user;
+//creat question Object/// not sure if i need to create answer object integrated with question or should i create anwser object on its own
+var questions [
+    {
+    question1: "",
+    answerA: "",
+    answerB: "",
+    answerC: "",
+    corectAns: "A"
+    },
+    {
+    question1: "",
+    answerA: "",
+    answerB: "",
+    answerC: "",
+    corectAns: "C"
+
+    },
+    {
+    question1: "",
+    answerA: "",
+    answerB: "",
+    answerC: "",
+    corectAns: "B"
+    }
+
+
+]
+
+
+//Get user information for user profile
+function getUserInfo(){
+    textBox.document.getElementById("#textBox", user)
+    prompt("What is your name?")
+    user = prompt
+}
+// WHEN I click the start button
+//Create a function for Start button(btn)
+function startGameBtn(){
+    startGameBtn
+    alert('lets play "Code Quiz"')
+
+}
+// THEN a timer starts 
+//Create a timer function 
+function startTimer(){
+    // THEN time is subtracted from the clock
+// }
+// I am presented with a question
+//Create a questions function
+function quizQuestions(){
+    
+    
+
+}
+// WHEN I answer a question
+//Create a function for anwsers
+function answers(){
+    answersBtn1.document.getElementById("answersBtn1")
+//     THEN I am presented with another question
+// WHEN I answer a question incorrectly
+}
+//create a Game over function
+function gameOver(){
+//     WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+
+
+// WHEN the game is over
+//Creat a score card function for user
+function getScore(){
+    // THEN I can save my initials and my score
+    user ++;
+}
+
+}
+
+
+
