9d84f4892d9126e159ffe43f1db5cae74b9a8556
Jonathan Johnson <jonathandila682@gmail.com>
2022-03-10 08:17:32 -0600

    Added css and js

--- script.js
+++ script.js
@@ -1,63 +1,99 @@
 // cCde Quiz
 //Dom Variables
-const startGameBtn = .document.getElementById('startGameBtn');
-const question = .document.getElementById('question');
-const quiz = .document.getElementById('quiz');
-const counter = .document.getElementById('counter');
-const timeGauge = .document.getElementById('timeGauge');
-const answerA = .document.getElementById('A');
-const answerB = .document.getElementById('B');
-const answerC = .document.getElementById('C');
+const startQuizBtn = document.getElementById('startQuizBtn');
+const questionBox = document.getElementById('questionBox');
+const nextBtn = document.getElementById('nextBtn');
+const counter = document.getElementById('counter');
+const answerA = document.getElementById('answerA');
+const answerB = document.getElementById('answerB');
+const answerC = document.getElementById('answerC');
+const score = document.getElementById('score');
+const user = document.getElementById('user');
+
+//Score variable
 
 //Store user info
-var user;
+
 //creat question Object//not sure if i need to create answer object integrated with question or should i create anwser object on its own
 var questions =[
     {
-    question1: "Who is the bootcamp instructor",
-    answerA: "Gary",
-    answerB: "Eugene",
-    answerC: "Nick",
-    correctAns: "A"
+    question: "Who is the bootcamp instructor?",
+    answers: ["Gary","Eugene","Nick"],
+    correctAns: 1
     },{
-    question1: "What college is the bootcamp hosted from?",
-    answerA: "University of texas",
-    answerB: "University of texas at Denton",
-    answerC: "Southern Methodist Univeristy",
-    corectAns: "C"
+    question: "What college is the bootcamp hosted from?",
+    answers: ["University of Texas","University of Texas at Denton","Southern Methodist University"],
+    correctAns: 3
     },{
-    question1: "What is a function?",
-    answerA: "A Template literal",
-    answerB: "set of statements that performs a task or calculates a value",
-    answerC: "A classification of data that tells the compiler or interpreter how the programmer wants to use the data.",
-    corectAns: "B"
+    question: "What is a function?",
+    answers: ["A Template literal","A set of statements that performs a task or calculates a value",
+    "A classification of data that tells the compiler or interpreter how the programmer wants to use the data."],
+    correctAns: 2
+    }, {
+    question: "Jonathan is the best programer in the entire world.",
+    answers: ["True","False","None of the above statements"],
+    correctAns: 1
+    }, {
+    question: "What is the syntax to declare a function named 'flashLight'?",
+    answers: ["myFunction flashLight","Function flashLight","function flashLight()"],
+    correctAns: 3
     }
 ]
 var secondsLeft = 15;
-
-
-
+function answers(){
+}
 //Get user information for user profile
 function getUserInfo(){
-    textBox.document.getElementById("#textBox", user)
     prompt("What is your name?")
     user = prompt
+}
+
+function quizQuestions() {
+  for (let i = 0; i < questions.length; i++) {
+      questionBox.innerHTML=this.i+1+" "+ this.questions[ithis.i].question
+      answerA.innerHTML=this.questions[this.questions.length].answers[0];
+      answerB.innerHTML=this.questions[this.questions.length].answers[1];
+      answerC.innerHTML=this.questions[this.questions.length].answers[2];
+        var response = window.prompt(questions[i].prompt);
+        if (response == questions[i].correctAns) {
+      score++;
+      alert("Correct!");
+    } else {
+      alert("Wrong!");
+    }
+  }
+  alert("You got " + score + "/" + questions.length);
+}
+function getScore() {
+  // THEN I can save my initials and my score
+  user.score;
+}
+
+function gameOver() {
+  prompt("Out of time! " + Y + N);
+  var choice = toLowerCase.Y || toLowerCase.N;
+  if (choice === Y) {
+    startGameBtn();
+  } else {
+    prompt("Have a nice day!");
+  }
+
+  // WHEN the game is over
+  //Creat a score card function for user
+  getScore();
 }
 //create a Game over function
 //WHEN I click the start button
 //Create a function for Start button(btn)
-function startGameBtn(){
-    startGameBtn
-    alert('lets play "Code Quiz"')
-
-
+function startGameBtnGenerate(){
+    alert('"Code Quiz")
+    quizQuestions();
 
 // THEN a timer starts 
 //Create a timer function 
-function startTimer(){
     // THEN time is subtracted from the clock
   // Sets interval in variable
-var timerInterval = setInterval(function() {
+var timerInterval = startTimer(function() {
     secondsLeft--;
     timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
 
@@ -65,50 +101,26 @@ var timerInterval = setInterval(function() {
       // Stops execution of action at set interval
     clearInterval(timerInterval);
       // Calls function for game over
-    gameOver;
+    gameOver();
     }
 
 }, 1000);
-}
+
 
 // I am presented with a question
 //Create a questions function
-function quizQuestions(){
-    questions
-    
-    
 
-}
 // WHEN I answer a question
 //Create a function for anwsers
 function answers(){
-    answersBtn1.document.getElementById("answersBtn1")
-//     THEN I am presented with another question
+    answersBtn.document.getElementById("answersBtn1")
+//THEN I am presented with another question
 // WHEN I answer a question incorrectly
 }
 
 //WHEN all questions are answered or the timer reaches 0
 //THEN the game is over
-function gameOver(){
 
-prompt("Out of time! Game Over! Would you like to play again?" + Y + N)
-var choice = toLowerCase.Y || toLowerCase.N
-if(choice === Y){
-    startGameBtn()
-} else {
-    prompt("Thank you for playing! Have a nice day!")
-}
-
-
-// WHEN the game is over
-//Creat a score card function for user
-function getScore(){
-    // THEN I can save my initials and my score
-    let score = user
-    user ++;
-}
-
-}
 }
 
 
